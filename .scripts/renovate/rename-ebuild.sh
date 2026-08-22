e9511ace3cce7a5bb1b7cf51c6a229a9280e2529
melody <admin@melthecat.dev>
2026-08-22 19:58:55 +0200

    update renovate config

---
+++ .scripts/renovate/rename-ebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+old_file=$1
+new_version=$2
+
+package_dir=$(dirname "$old_file")
+package_name=$(basename "$package_dir")
+
+new_file="${package_dir}/${package_name}-${new_version}.ebuild"
+
+mv "$old_file" "$new_file"
