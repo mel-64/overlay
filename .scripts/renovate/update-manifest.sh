6c7908cdd710d6ef8eb064c7dad34e3568cc1f5f
melody <admin@melthecat.dev>
2026-08-23 23:04:09 +0200

    fix incorrectly generating thick manifests

--- .scripts/renovate/update-manifest.sh
+++ .scripts/renovate/update-manifest.sh
@@ -26,7 +26,7 @@ curl -fsSL "$mirror/eclass/Manifest.gz" -o "$work/eclass-manifest.gz"
 zcat "$work/eclass-manifest.gz" | awk '$2 ~ /\.eclass$/ {print $2}' \
     | xargs -P8 -I{} curl -fsSL "$mirror/eclass/{}" -o "$work/eclass/{}"
 
-printf 'masters =\nrepo-name = %s\n' "$repo_name" > "$work/metadata/layout.conf"
+printf 'masters =\nthin-manifests = true\nrepo-name = %s\n' "$repo_name" > "$work/metadata/layout.conf"
 printf '%s\n' "$repo_name" > "$work/profiles/repo_name"
 printf '%s\n' "$category" > "$work/profiles/categories"
 printf 'ARCH="amd64"\nCHOST="x86_64-pc-linux-gnu"\n' > "$work/profiles/default/make.defaults"
